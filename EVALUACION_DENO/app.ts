5561c85aca2f56315eff3b1430e4d559c7329237
Santiago Ortiz <santiagoortizgomez621@gmail.com>
2026-09-15 13:22:33 -0500

    Deno estructura

---
+++ EVALUACION_DENO/app.ts
@@ -0,0 +1,18 @@
+import { Application, oakCors } from "./Dependencies/dependencies.ts";
+const app = new Application();
+await load({export: true});
+app.use(oakCors({
+    origin:"*"
+}));
+
+
+const routes = [];
+
+routes.forEach(router =>{
+    app.use(router.routes());
+    app.use(router.allowedMethods());
+})
+
+console.log("Servidor corriendo por el puerto 8001");
+
+app.listen({port: 8001});
